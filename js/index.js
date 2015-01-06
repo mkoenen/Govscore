99627723355af781c95f6adaeba593d940f35857
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-01-06 12:45:48 -0500

    Revert 661cdd6..52007f5
    This rolls back to commit 661cdd61cc62f43a6582ce07db3c71622b37afc1.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,64 @@
-function init() {
+
+   /* document.addEventListener('deviceready', notificationReady, false);
+    
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    function notificationReady() {
+       receivedEvent('deviceready');
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(successHandler, errorHandler,{"senderID":"999347728559","ecb":"app.onNotificationGCM"});
+
+    }
+    // Update DOM on a Received Event
+    function receivedEvent(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+    // result contains any message sent from the plugin call
+    function successHandler(result) {
+        alert('Callback Success! Result = '+result)
+    }
+    function errorHandler(error) {
+        alert(error);
+    }
+    function onNotificationGCM(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+                break;
+
+            case 'message':
+                // this is the actual push notification. its format depends on the data model from the push server
+                alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+                break;
+
+            case 'error':
+                alert('GCM error = '+e.msg);
+                break;
+
+            default:
+                alert('An unknown GCM event has occurred');
+                break;
+        }
+    }*/
+
+ function init() {
     document.addEventListener("deviceready", initPushwoosh, true);
  
     //rest of the code
-} 
- 
+}   
 function initPushwoosh()
 {
     var pushNotification = window.plugins.pushNotification;
