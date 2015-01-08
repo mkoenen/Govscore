6cc34b4218b83f2dc00d9b31b3cecaeb341e071d
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-01-08 10:09:00 -0500

    get open close all in place
    add css and open close functions, delete alerts

--- js/initialscripts.js
+++ js/initialscripts.js
@@ -8,10 +8,10 @@ function listenOpenClose() {
     document.getElementById('close-cq1').addEventListener('click', closeIsClicked, false);
 }
 function openIsClicked() {
-    alert("clicked hide");
+    $( ".questions").hide();
 } 
 function closeIsClicked() {
-    alert("clicked show");
+    $( ".questions").show();
 }          
 function setbutton() {
     document.getElementById('btnStore').addEventListener('click', saveIsClicked, false);
