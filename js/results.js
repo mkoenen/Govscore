cb9f99172bc55c437c2e84c7f484b144e90a2d2f
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-01-21 13:30:08 -0500

    fix

--- js/results.js
+++ js/results.js
@@ -92,7 +92,7 @@ function calcResults(){
     eval += "<h2>Recommendation</h2><p>The areas that your organization should focus on are, in order,</p>"
 
    for (i = 0; i < weakest.length; i++){
-    eval += "<h3>" + weakest[i] + "</h2>;
+    eval += "<h3>" + weakest[i] + "</h3>;
    }
 
     //alert("eval is " + eval);
