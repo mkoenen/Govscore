fbd17575f33ac25ca7581475d970bd3e46a5cc04
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-01-20 21:40:40 -0500

    pull from storage

--- js/results.js
+++ js/results.js
@@ -8,9 +8,35 @@ var accScore, stakeScore, dirScore, resScore, enhanceScore
 
 //add up the numbers
 function calcResults(){
-	alert(g1);
-	alert(g13);
-	alert(g1 + g13);
+
+	var g1 = window.localStorage.getItem("answer1");
+    var g2 = window.localStorage.getItem("answer2");
+    var g3 = window.localStorage.getItem("answer3");
+    var g4 = window.localStorage.getItem("answer4");
+    var g5 = window.localStorage.getItem("answer5");
+    var g6 = window.localStorage.getItem("answer6");
+    var g7 = window.localStorage.getItem("answer7");
+    var g8 = window.localStorage.getItem("answer8");
+    var g9 = window.localStorage.getItem("answer9");
+    var g10 = window.localStorage.getItem("answer10");
+    var g11 = window.localStorage.getItem("answer11");
+    var g12 = window.localStorage.getItem("answer12");
+    var g13 = window.localStorage.getItem("answer13");
+    var g14 = window.localStorage.getItem("answer14");
+    var g15 = window.localStorage.getItem("answer15");
+    var g16 = window.localStorage.getItem("answer16");
+    var g17 = window.localStorage.getItem("answer17");
+    var g18 = window.localStorage.getItem("answer18");
+    var g19 = window.localStorage.getItem("answer19");
+    var g20 = window.localStorage.getItem("answer20");
+    var g21 = window.localStorage.getItem("answer21");
+    var g22 = window.localStorage.getItem("answer22");
+    var g23 = window.localStorage.getItem("answer23");
+    var g24 = window.localStorage.getItem("answer24");
+    var g25 = window.localStorage.getItem("answer25");
+
+
+	
 	accScore = g1+g2+g5+g8+g10+g13;
 	var accPossible = 24;
 	stakeScore = g11+g14+g22;
