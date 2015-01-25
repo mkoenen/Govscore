e30ffde864af17501db31643af3121d56fc02270
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-01-25 17:50:06 -0500

    remove used results from weakest array

--- js/main.js
+++ js/main.js
@@ -327,7 +327,6 @@ function saveServer() {
 
     var getSaved = window.localStorage.getItem("saved");
     var savedName = window.localStorage.getItem("name");
-    //alert( "saved is " + getSaved + "and name is " + savedName); //temp
     if (getSaved !== "true" && savedName !== null ) {
 
        
@@ -365,9 +364,7 @@ function saveServer() {
 
         saved = window.localStorage.setItem("saved", "true");
 
-
-        alert(g1 + "," + g2);
-
+        //send to server
         var data = { "date" : gsdate, "name": name, "email": email, "organization": organization, "g1": g1, "g2": g2, "g3": g3, "g4": g4, "g5": g5, "g6": g6, "g7": g7, "g8": g8, "g9": g9, "g10": g10, "g11": g11, "g12": g12, "g13": g13, "g14": g14, "g15": g15, "g16": g16, "g17": g17, "g18": g18, "g19": g19, "g20": g20, "g21": g21, "g22": g22, "g23": g23, "g24": g24, "g25": g25  };
         
         $.ajax({
@@ -1102,31 +1099,7 @@ function checkResults(){
 }
 //add up the numbers
 function calcResults(){
-        var g1 = window.localStorage.getItem("answer1");
-        var g2 = window.localStorage.getItem("answer2");
-        var g3 = window.localStorage.getItem("answer3");
-        var g4 = window.localStorage.getItem("answer4");
-        var g5 = window.localStorage.getItem("answer5");
-        var g6 = window.localStorage.getItem("answer6");
-        var g7 = window.localStorage.getItem("answer7");
-        var g8 = window.localStorage.getItem("answer8");
-        var g9 = window.localStorage.getItem("answer9");
-        var g10 = window.localStorage.getItem("answer10");
-        var g11 = window.localStorage.getItem("answer11");
-        var g12 = window.localStorage.getItem("answer12");
-        var g13 = window.localStorage.getItem("answer13");
-        var g14 = window.localStorage.getItem("answer14");
-        var g15 = window.localStorage.getItem("answer15");
-        var g16 = window.localStorage.getItem("answer16");
-        var g17 = window.localStorage.getItem("answer17");
-        var g18 = window.localStorage.getItem("answer18");
-        var g19 = window.localStorage.getItem("answer19");
-        var g20 = window.localStorage.getItem("answer20");
-        var g21 = window.localStorage.getItem("answer21");
-        var g22 = window.localStorage.getItem("answer22");
-        var g23 = window.localStorage.getItem("answer23");
-        var g24 = window.localStorage.getItem("answer24");
-        var g25 = window.localStorage.getItem("answer25");
+        alert(g1 + "," + g22);
 
     var percentArray = new Array();
 
@@ -1177,10 +1150,14 @@ function calcResults(){
     }
 
     var sortedPercent = percentArray.sort(compareNumbers);
-    if(sortedPercent[0]==sortedPercent[1] || sortedPercent[1] == sortedPercent[2]){
-        var weakest = sortedPercent.slice(0,3);
-    }else{
+    if(sortedPercent[0]!=sortedPercent[1] && sortedPercent[1] != sortedPercent[2]){
         var weakest = sortedPercent.slice(0,2);
+    }else if(sortedPercent[0]==sortedPercent[1] || sortedPercent[1] == sortedPercent[2]){
+        var weakest = sortedPercent.slice(0,3);
+    }else if (sortedPercent[0]==sortedPercent[1] || sortedPercent[1] == sortedPercent[2]) && sortedPercent[2] == sortedPercent[3]){
+        var weakest = sortedPercent.slice(0,4);
+    }else {
+        var weakest = sortedPercent;
     }
 
     var eval = "<h2>Govscore Assessment</h2><p>Overall your Organization scores as follows: </p>";
@@ -1196,18 +1173,23 @@ function calcResults(){
         switch(weakest[i]){
             case accPercent:
                 eval += "<li>" + (i+1) + ". Cultivating Accountability (" + accPercent + "%) </li>";
+                weakest.splice(i,1);
                 break;
             case stakePercent:
                 eval += "<li>" + (i+1) + ". Engaging Stakeholders (" + stakePercent + "%)</li>";
+                weakest.splice(i,1);
                 break;
             case dirPercent:
                 eval += "<li>" + (i+1) + ". Shared Strategic Direction (" + dirPercent + "%)</li>";
+                weakest.splice(i,1);
                 break;
             case resPercent:
                 eval += "<li>" + (i+1) + ". Stewarding Resources (" + resPercent + "%)</li>";
+                weakest.splice(i,1);
                 break;
             case enhPercent:
                 eval += "<li>" + (i+1) + ". Continuous Governance Enhancement (" + enhPercent + "%)</li>";
+                weakest.splice(i,1);
                 break;
         }
    }
