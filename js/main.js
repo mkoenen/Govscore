9de3383f02549d80ce32df72649f0283a9e9d243
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-01-28 09:07:58 -0500

    back to GET

--- js/main.js
+++ js/main.js
@@ -366,7 +366,7 @@ function saveServer() {
         var data = { "date" : gsdate, "name": name, "email": email, "organization": organization, "g1": g1, "g2": g2, "g3": g3, "g4": g4, "g5": g5, "g6": g6, "g7": g7, "g8": g8, "g9": g9, "g10": g10, "g11": g11, "g12": g12, "g13": g13, "g14": g14, "g15": g15, "g16": g16, "g17": g17, "g18": g18, "g19": g19, "g20": g20, "g21": g21, "g22": g22, "g23": g23, "g24": g24, "g25": g25  };
         
         $.ajax({
-            type       : "POST",
+            type       : "GET",
             url        : "http://sensi.wpengine.com/store.php",
             crossDomain: true,
             data       : JSON.stringify(data),
