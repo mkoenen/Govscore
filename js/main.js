d42495925170e0b1a4ebaa591e244a4e443d0a67
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-01-28 08:59:03 -0500

    try POST one more time

--- js/main.js
+++ js/main.js
@@ -232,7 +232,6 @@ function savelocal() {
     
     var newdate = new Date();
     gsdate = formatDate(newdate);
-    alert(gsdate);
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var organization =  $( "#organization" ).val();
@@ -364,12 +363,10 @@ function saveServer() {
         saved = window.localStorage.setItem("saved", "true");
 
 
-        alert(g1 + "," + g2);
-
         var data = { "date" : gsdate, "name": name, "email": email, "organization": organization, "g1": g1, "g2": g2, "g3": g3, "g4": g4, "g5": g5, "g6": g6, "g7": g7, "g8": g8, "g9": g9, "g10": g10, "g11": g11, "g12": g12, "g13": g13, "g14": g14, "g15": g15, "g16": g16, "g17": g17, "g18": g18, "g19": g19, "g20": g20, "g21": g21, "g22": g22, "g23": g23, "g24": g24, "g25": g25  };
         
         $.ajax({
-            type       : "GET",
+            type       : "POST",
             url        : "http://sensi.wpengine.com/store.php",
             crossDomain: true,
             data       : JSON.stringify(data),
