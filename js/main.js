ea8d3718cfb3665f4380e9f04d8812e3117ad4f8
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-01-29 08:58:41 -0500

    window.onload function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 /* Events -----------------------------------------*/
 
-function init() {
+window.onload = function(){
     //window.setTimeout(beonline, 6000);
     document.addEventListener("online", onOnline, true);                               //limit how fast the online event can fire
     document.addEventListener("deviceready", setbutton, false);
