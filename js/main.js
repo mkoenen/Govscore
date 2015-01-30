5c422a23463e602678b851a63a3596b529e60ad7
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-01-30 14:24:38 -0500

    test

--- js/main.js
+++ js/main.js
@@ -352,11 +352,12 @@ function saveServer() {
 
         gsSaved = window.localStorage.setItem("saved", "true");
 
-        alert(data);
+        //alert(data);
+        
+        data = { "date" : gsdate, "name": name, "email": email, "organization": organization, "g1": g1, "g2": g2, "g3": g3, "g4": g4, "g5": g5, "g6": g6, "g7": g7, "g8": g8, "g9": g9, "g10": g10, "g11": g11, "g12": g12, "g13": g13, "g14": g14, "g15": g15, "g16": g16, "g17": g17, "g18": g18, "g19": g19, "g20": g20, "g21": g21, "g22": g22, "g23": g23, "g24": g24, "g25": g25  };
         data= JSON.stringify(data);
         alert(data);
-        //data = { "date" : gsdate, "name": name, "email": email, "organization": organization, "g1": g1, "g2": g2, "g3": g3, "g4": g4, "g5": g5, "g6": g6, "g7": g7, "g8": g8, "g9": g9, "g10": g10, "g11": g11, "g12": g12, "g13": g13, "g14": g14, "g15": g15, "g16": g16, "g17": g17, "g18": g18, "g19": g19, "g20": g20, "g21": g21, "g22": g22, "g23": g23, "g24": g24, "g25": g25  };
-        
+
         $.ajax({
             type       : "GET",
             url        : "http://sensi.wpengine.com/store.php",
