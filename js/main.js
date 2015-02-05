e6fc5be7f5ca55a633a8e95af498a134fe13c28f
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-02-05 13:27:50 -0500

    quotes

--- js/main.js
+++ js/main.js
@@ -268,7 +268,7 @@ function saveServer() {
     var gsdata;
 
     //get the data from local storage
-    gsdata = localStorage.getObject(gsdata);
+    gsdata = localStorage.getObject('gsdata');
 
     saveToServer("http://sensi.wpengine.com/store.php", gsdata, "gsSaved");
 
