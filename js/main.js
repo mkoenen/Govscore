4e4bc5f669b178b71b7ffbf6eb874e14f9d65b85
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-02-23 18:54:19 -0500

    add pushwoosh, don't limit to android

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.onload = function(){
     document.addEventListener("deviceready", onOnline, true); 
     document.addEventListener("deviceready", showResults, false);
     document.addEventListener("deviceready", setbuttons, false);
-    //document.addEventListener("deviceready", initPushwoosh, true);
+    document.addEventListener("deviceready", initPushwoosh, true);
     document.addEventListener("deviceready", showResultsButtons, false);
     document.addEventListener("deviceready", onOnline, true);
 };
@@ -673,5 +673,40 @@ function calcResults() {
     }
     localStorage.setItem("result", res);
     document.getElementById('gs-results').innerHTML = res; 
+}
+
+
+
+/* Pushwoosh ---------------------------------------------------*/
+
+function initPushwoosh()
+{
+    var pushNotification = window.plugins.pushNotification;
+ 
+    //set push notifications handler
+    document.addEventListener('push-notification', function(event) {
+        var title = event.notification.title;
+        var userData = event.notification.userdata;
+                                 
+        if(typeof(userData) != "undefined") {
+            console.warn('user data: ' + JSON.stringify(userData));
+        }
+                                     
+        alert(title);
+    });
+ 
+    //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", pw_appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+    pushNotification.onDeviceReady({ projectid: "govscore-100", pw_appid : "4C804-675D6" });
+ 
+    //register for pushes
+    pushNotification.registerDevice(
+        function(status) {
+            var pushToken = status;
+            console.warn('push token: ' + pushToken);
+        },
+        function(status) {
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
 }
 
